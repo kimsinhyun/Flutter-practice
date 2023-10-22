d61c3cf48622796288ed953efb6ff6f5c8754337
kimsinhyun <ksh415622@naver.com>
2023-10-22 18:10:12 +0900

    refactor: hourly card 분리

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_hello_world/component/main_stat.dart';
 
 import '../component/card_title.dart';
 import '../component/category_card.dart';
+import '../component/hourly_card.dart';
 import '../const/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -26,56 +27,7 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     CategoryCard(),
                     const SizedBox(height: 16.0),
-                    MainCard(
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                            CardTitle(title: "시간별 미세먼지"),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16.0,
-                              vertical: 8.0),
-                          child: Column(
-                              children: List.generate(
-                                  24, (index) {
-                                  final now = DateTime.now();
-                                  int currentHour = now.hour - index;
-
-                                  if (currentHour < 0) {
-                                    currentHour = 24 + currentHour;
-                                  }
-
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 8.0,
-                                      vertical: 4.0,
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Expanded(
-                                          child: Text('$currentHour시'),
-                                        ),
-                                        Expanded(
-                                          child: Image.asset(
-                                            'asset/img/good.png',
-                                            height: 20.0,
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: Text(
-                                            '${index * 10} ㎍/㎥',
-                                            textAlign: TextAlign.right,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  );
-                              }),
-                        )
-                    )
-                  ],
-                ),
-              )
+                    HourlyCard(),
             ],
           ))]
       ,
